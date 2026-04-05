6a2a3601d365ebeabda95316effc9601af8ab30f
AlfredYH <xu_yuanhao@126.com>
2026-04-05 22:22:38 +0800

    modify details in ignore file

--- main_test.py
+++ main_test.py
@@ -1,5 +1,9 @@
 from trader.trader import TradeOperator
 from utils.daily_update import DailyUpdate
+import os
+import re
+
+
 
 if __name__ == "__main__":
     # operator = TradeOperator()
@@ -11,4 +15,28 @@ if __name__ == "__main__":
     dataupdate = DailyUpdate()
     # dataupdate.get_all_stock_list() # 获取股票列表并保存到CSV文件
     dataupdate.run_sync_process(adjust_type="bfq", debug_mode=True) # 同步不复权数据（bfq）
+
+
+
+
+
+
+
+
+    #################################################################
+    # 每日删除当前文件夹下的 libhv.数字.log 文件，防止日志文件过多占用空间
+    #################################################################
+
+
+    # 匹配规则：libhv + 点 + 纯数字 + 点 + log
+    pattern = re.compile(r"^libhv\.\d+\.log$")
+
+    # 遍历当前文件夹
+    for filename in os.listdir("."):
+        if pattern.match(filename):
+            try:
+                os.remove(filename)
+                print(f"✅ 已删除: {filename}")
+            except Exception as e:
+                print(f"❌ 删除失败 {filename}: {e}")
 
